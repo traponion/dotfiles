3d25ef7ca231e61d3d46a97310a0cca7bce13254
traponion <6.6.6.trap@gmail.com>
2024-04-27 16:38:01 +0900

    インストールスクリプトとnvimの設定ファイルを更新

--- install.sh
+++ install.sh
@@ -12,15 +12,41 @@ NOW=$(date '+%Y%m%d%H%M%S')
 # バックアップ用のサフィックス
 BACKUP_SUFFIX=".bk_$NOW"
 
+# WSLかどうかを判定
+if [[ "$(uname -r)" =~ "WSL" ]]; then
+    if ! command -v wl-copy &> /dev/null
+    then
+        echo "Installing wl-clipboard..."
+        sudo apt-get update
+        sudo apt-get install -y wl-clipboard
+    fi
+fi
+
+# Homebrew がインストールされていない場合はインストール
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Neovim がインストールされていない場合はインストール
+if ! command -v nvim &> /dev/null
+then
+    echo "Installing Neovim..."
+    brew install neovim
+fi
+
 # nvimの設定ファイルのバックアップ（存在する場合）
 if [ -d $NVIM_CONFIG_PATH ]; then
     echo "Backing up existing nvim config..."
     mv $NVIM_CONFIG_PATH $NVIM_CONFIG_PATH$BACKUP_SUFFIX
-else
-    echo "Creating nvim config directory..."
-    mkdir -p $NVIM_CONFIG_PATH
 fi
+
+echo "Creating nvim config directory..."
+mkdir -p $NVIM_CONFIG_PATH
 
 # nvimの設定ファイルのシンボリックリンクを作成
 echo "Creating nvim config symlink..."
 ln -s $DOTFILES_PATH/nvim/init.lua $NVIM_CONFIG_PATH/init.lua
+
+echo "Done!"
